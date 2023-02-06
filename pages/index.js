cd8424a4e59a317dd3cd91eee34f3df7c148bfb3
unknown <shvydchenkoandriy@gmail.com>
2023-02-06 20:27:26 +0200

    desktop version

--- pages/index.js
+++ pages/index.js
@@ -27,8 +27,7 @@ export default function Home() {
       },
     ],
   };
-  const storageVal = useRef(null);
-  const transferVal = useRef(null);
+
   const [storage, setStorage] = useState(100);
   const [transfer, setTransfer] = useState(400);
   const [bunnyStorage, setBunnyStorage] = useState({ hdd: true, ssd: false });
@@ -38,22 +37,18 @@ export default function Home() {
   });
 
   const handlerStorageVal = (e) => {
-    setStorage((prev) => (prev = e.target.value));
+    setStorage(e.target.value);
   };
 
   const handlerTransferVal = (e) => {
-    setTransfer((prev) => (prev = e.target.value));
+    setTransfer(e.target.value);
   };
 
   const resultBacklazeCom = () => {
     const result = providers.backlazeCom.map(
       (item) => item.storagePrice * storage + item.transferPrice * transfer
     );
-    if (result[0] < 7) {
-      return 7;
-    } else {
-      return result[0].toFixed(2);
-    }
+    return Math.max(7, result[0]).toFixed(2);
   };
 
   const resultBunnyNet = () => {
@@ -63,24 +58,21 @@ export default function Home() {
           storage +
         item.transferPrice * transfer
     );
-    if (result[0] > 10) {
-      return 10;
-    } else {
-      return result[0].toFixed(2);
-    }
+
+    return Math.min(10, result[0]).toFixed(2);
   };
 
   const resultScalewayCom = () => {
     const result = providers.scalewayCom.map((item) => {
-      if (storage < 75 || transfer < 75) {
+      if (storage <= 75 || transfer <= 75) {
         return 0;
       } else {
         return (
           (scalewayStorage.multi
             ? item.storagePriceMulti
             : item.storagePriceSingle) *
-            storage +
-          item.transferPrice * transfer
+            (storage - 75) +
+          item.transferPrice * (transfer - 75)
         );
       }
     });
@@ -91,122 +83,143 @@ export default function Home() {
     const result = providers.vultrCom.map(
       (item) => item.storagePrice * storage + item.transferPrice * transfer
     );
-    if (result[0] < 5) {
-      return 5;
-    } else {
-      return result[0].toFixed(2);
-    }
+
+    return Math.max(5, result[0]).toFixed(2);
   };
   console.log(bunnyStorage);
   return (
-    <div>
+    <div className="flex items-start m-10 static w-full">
       <Head>
         <title>Providers calculator</title>
       </Head>
       <div>
-        <input
-          ref={storageVal}
-          type="range"
-          id="storage"
-          name="storage"
-          min={0}
-          max={1000}
-          step={1}
-          defaultValue={storage}
-          onChange={() => handlerStorageVal(event)}
-        ></input>
-        Storage {storage}
-      </div>
-      <div>
-        <input
-          ref={transferVal}
-          type="range"
-          id="transfer"
-          name="transfer"
-          step={1}
-          min={0}
-          max={1000}
-          defaultValue={transfer}
-          onChange={() => handlerTransferVal(event)}
-        ></input>
-        Transfer {transfer}
-      </div>
-      <div>
-        <div className="flex gap-1">
-          <span>backlaze.com</span>
-          <div
-            className={`bg-green`}
-            style={{ width: `${resultBacklazeCom() * 5}px` }}
-          ></div>
-          <div>{resultBacklazeCom()}$</div>
+        <div className="flex justify-around gap-10 absolute">
+          <div className="flex flex-col">
+            Storage: {storage} GB
+            <input
+              type="range"
+              id="storage"
+              name="storage"
+              min={0}
+              max={1000}
+              step={1}
+              defaultValue={storage}
+              onChange={(event) => handlerStorageVal(event)}
+            ></input>
+          </div>
+          <div className="flex flex-col">
+            Transfer: {transfer} GB
+            <input
+              type="range"
+              id="transfer"
+              name="transfer"
+              step={1}
+              min={0}
+              max={1000}
+              defaultValue={transfer}
+              onChange={(event) => handlerTransferVal(event)}
+            ></input>
+          </div>
         </div>
-        <div className="flex gap-2">
-          <div>
-            <div>bunny.net</div>
+        <div className="flex absolute top-[100px]">
+          <div className="flex flex-col gap-10">
             <div>
-              <input
-                onChange={() => setBunnyStorage({ hdd: true, sdd: false })}
-                type="radio"
-                id="bunnyChoice1"
-                name="bunny"
-                value="hdd"
-              ></input>
-              <label htmlFor="bunnyChoice1">HDD</label>
-              <input
-                onChange={() => setBunnyStorage({ hdd: false, sdd: true })}
-                type="radio"
-                id="bunnyChoice2"
-                name="bunny"
-                value="sdd"
-              ></input>
-              <label htmlFor="bunnyChoice2">SDD</label>
+              <div>backlaze.com</div>
+            </div>
+            <div>
+              <div>bunny.net</div>
+              <div>
+                <label>
+                  <input
+                    onChange={() => setBunnyStorage({ hdd: true, sdd: false })}
+                    type="radio"
+                    id="hdd"
+                    name="hdd-ssd"
+                    value="hdd"
+                    checked={bunnyStorage.hdd}
+                  ></input>
+                  HDD
+                </label>
+                <label>
+                  <input
+                    onChange={() => setBunnyStorage({ hdd: false, sdd: true })}
+                    type="radio"
+                    id="ssd"
+                    name="hdd-ssd"
+                    value="sdd"
+                  ></input>
+                  SDD
+                </label>
+              </div>
+            </div>
+            <div>
+              <div>scaleway.com</div>
+              <div>
+                <input
+                  onChange={() =>
+                    setScalewayStorage({ multi: true, single: false })
+                  }
+                  type="radio"
+                  id="scalewayChoice1"
+                  name="scaleway"
+                  value="multi"
+                  checked={scalewayStorage.multi}
+                ></input>
+                <label htmlFor="scalewayChoice1" className="p-2">
+                  Multi
+                </label>
+                <input
+                  onChange={() =>
+                    setScalewayStorage({ multi: false, single: true })
+                  }
+                  type="radio"
+                  id="scalewayChoice2"
+                  name="scaleway"
+                  value="single"
+                ></input>
+                <label htmlFor="scalewayChoice2">Single</label>
+              </div>
+            </div>
+            <div>
+              <span>vultr.com</span>
             </div>
           </div>
-          <div
-            className={`bg-green h-[50px]`}
-            style={{ width: `${resultBunnyNet() * 5}px` }}
-          ></div>
-          <div>{resultBunnyNet()}$</div>
-        </div>
-        <div className="flex gap-2">
-          <div>
-            <div>scaleway.com</div>
-            <div>
-              <input
-                onChange={() =>
-                  setScalewayStorage({ multi: true, single: false })
-                }
-                type="radio"
-                id="scalewayChoice1"
-                name="scaleway"
-                value="multi"
-              ></input>
-              <label htmlFor="scalewayChoice1">Multi</label>
-              <input
-                onChange={() =>
-                  setScalewayStorage({ multi: false, single: true })
-                }
-                type="radio"
-                id="scalewayChoice2"
-                name="scaleway"
-                value="single"
-              ></input>
-              <label htmlFor="scalewayChoice2">Single</label>
+          <div className="flex flex-col items-start border-l-2 relative">
+            <div className="flex absolute">
+              <div
+                className={`bg-green`}
+                style={{
+                  width: `${resultBacklazeCom() * 5}px`,
+                  height: "25px",
+                }}
+              ></div>
+              <div>{resultBacklazeCom()}$</div>
+            </div>
+            <div className="flex absolute top-[70px]">
+              <div
+                className={`bg-green`}
+                style={{ width: `${resultBunnyNet() * 5}px`, height: "30px" }}
+              ></div>
+              <div>{resultBunnyNet()}$</div>
+            </div>
+            <div className="flex absolute top-[160px]">
+              <div
+                className={`bg-green`}
+                style={{
+                  width: `${resultScalewayCom() * 5}px`,
+                  height: "30px",
+                }}
+              ></div>
+              <div>{resultScalewayCom()}$</div>
+            </div>
+            <div className="flex absolute top-[240px]">
+              <div
+                className={`bg-green `}
+                style={{ width: `${resultVultrCom() * 5}px`, height: "25px" }}
+              ></div>
+              <div>{resultVultrCom()}$</div>
             </div>
           </div>
-          <div
-            className={`bg-green h-[50px]`}
-            style={{ width: `${resultScalewayCom() * 5}px` }}
-          ></div>
-          <div>{resultScalewayCom()}$</div>
-        </div>
-        <div className="flex gap-1">
-          <span>vultr.com</span>
-          <div
-            className={`bg-green`}
-            style={{ width: `${resultVultrCom() * 5}px` }}
-          ></div>
-          <div>{resultVultrCom()}$</div>
         </div>
       </div>
     </div>
