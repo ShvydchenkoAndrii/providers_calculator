337e8d3b77027cb3b232363cf3f049e0a22d3a78
ShvydchenkoAndrii <107943027+ShvydchenkoAndrii@users.noreply.github.com>
2023-02-08 11:34:33 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,8 @@ module.exports = {
   theme: {
     colors: {
       white: "#ffffff",
-      main: "#f5f5f5",
-      black: "#000000",
       red: "#800000",
-      h1: "rgba(175, 47, 47, 0.15)",
-      grey: "#13ce66",
-      green: "#50C878",
-      input: "rgba(0, 0, 0, 0.003)",
       gray: "#8492a6",
-      "gray-light": "#e6e6e6",
     },
     extend: {},
   },
